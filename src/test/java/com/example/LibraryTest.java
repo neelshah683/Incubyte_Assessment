db359f423a77dd4a162f5290d9141b1081254382
NEEL <neelshah682003@gmail.com>
2024-08-30 21:42:42 +0530

    Implement Add Book feature

--- src/test/java/com/example/LibraryTest.java
+++ src/test/java/com/example/LibraryTest.java
@@ -14,6 +14,9 @@ public class LibraryTest {
     @BeforeEach
     public void init(){
         library = new Library();
+
+        System.out.println("initial config:");
+
         book1 = new Book("1A", "Introduction to JAVA", "James Gosling", 1995);
         book2 = new Book("1B", "Introduction to Python", "Van Rossum", 1991);
         library.addBook(book1);
@@ -23,6 +26,7 @@ public class LibraryTest {
     public void addBookTest(){
         Book book3 = new Book("1C", "Introduction to C++", "Bjarne Stroustrup", 1979);
         library.addBook(book3);
+        
         assertEquals(3, library.viewBooks().size());
     }
 }
